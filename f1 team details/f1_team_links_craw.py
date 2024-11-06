2390762c5ac247153a0b2741d6494443d52fbd48
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-11-06 15:25:19 +0700

    New File

--- f1 team details/f1_team_links_craw.py
+++ f1 team details/f1_team_links_craw.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+#
 url = 'https://www.formula1.com/en/teams.html'
 response = requests.get(url)
 print (response)
@@ -18,6 +19,7 @@ for tag_link in tag_links:
     href = tag_link.get('href')  # Lấy giá trị của thuộc tính href
     if href and href.startswith('/en/teams/'):
         href_links.append(f'{base_url}'+href+'.html')
+
 print(href_links)
 
 
