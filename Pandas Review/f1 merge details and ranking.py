b8fea4088b1d46fae9c452754cf320097ab94792
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-11-13 14:12:36 +0700

    Pandas review

--- Pandas Review/f1 merge details and ranking.py
+++ Pandas Review/f1 merge details and ranking.py
@@ -51,3 +51,47 @@ query_4_2nd = merged_df.groupby("Team").agg(
 
 query_4_2nd = query_4_2nd[query_4_2nd["total_championships"] > 0]
 print(query_4_2nd)
+
+print ("------------------------------------------------------------")
+
+# mean podium
+mean_podium = merged_df["Podiums"].mean()
+print (mean_podium)
+print ("------------------------------------------------------------")
+
+# meam points 
+mean_points = merged_df["Points"].mean()
+print (mean_points)
+print ("------------------------------------------------------------")
+
+# các team có tông điểm lớn hơn mean, cả số lần win podium
+query_5 = merged_df.groupby("Team").agg(
+        above_mean = pd.NamedAgg(column ="Points", aggfunc = lambda x : (x.sum() > mean_points)),
+        Podiums_win  = pd.NamedAgg(column = "Podiums", aggfunc = "sum")
+        )
+
+print (query_5)
+print ("------------------------------------------------------------")
+
+# tìm các tay đua có top 1 trên 20 lần
+# Split 'Highest race finish' to get the top position and the count
+merged_df[['Top_Position', 'Times']] = merged_df['Highest race finish'].str.extract(r'(\d) \(x(\d+)\)')
+# Convert the 'Times' column to integer for comparison
+merged_df['Times'] = merged_df['Times'].astype(int)
+
+# Filter for drivers who finished at position 1 more than 20 times
+query_6 = merged_df[(merged_df['Top_Position'] == '1') & (merged_df['Times'] > 20)]
+print(query_6[['Driver', 'Highest race finish']])
+
+print ("------------------------------------------------------------")
+
+# in các tay đua chưa có top 1 
+query_7 = merged_df[merged_df["Top_Position"] > '1']
+print(query_7[["Driver", "Highest race finish"]])
+print ("------------------------------------------------------------")
+
+# kiểm tra xem các tay đua của từng team có đạt top 1 hay chưa
+query_8 = merged_df.groupby(["Team", "Driver"]).agg(
+    top1_or_not = pd.NamedAgg(column = "Top_Position" , aggfunc = lambda x:(x == '1'))
+        )
+print (query_8)
